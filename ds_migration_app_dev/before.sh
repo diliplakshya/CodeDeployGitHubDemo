5c9904e66ea435837732fc36dd6cf281c6582309
Dilip Kumar Sharma <diliplakshya@gmail.com>
2020-11-30 23:45:29 +0530

    comment

--- ds_migration_app_dev/before.sh
+++ ds_migration_app_dev/before.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Before install"
-sudo yum install -y python3-devel
-sudo yum install -y python3.7
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
-pip --version
+# sudo yum install -y python3-devel
+# sudo yum install -y python3.7
+# curl -O https://bootstrap.pypa.io/get-pip.py
+# python3 get-pip.py --user
+# pip --version
